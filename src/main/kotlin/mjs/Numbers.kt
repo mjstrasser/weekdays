61f3fc19149b4087d4737a95ec47e9056eadd98f
Michael Strasser <michael@michaelstrasser.com>
2021-02-03 18:27:20 +1000

    Working solution

--- src/main/kotlin/mjs/Numbers.kt
+++ src/main/kotlin/mjs/Numbers.kt
@@ -1,12 +1,16 @@
 package mjs
 
-val WEEKDAYS = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+fun uniqueIds(words: Set<String>): Set<Char> {
 
-fun uniqueIds(weekdays: List<String>): Set<Char> {
+    fun findUnique(word: String, otherWords: Set<String>): Char {
 
-    val firstLetters = weekdays.map { it.first() }
-    val secondLetters = weekdays.map { it[1] }
-    val thirdLetters = weekdays.map { it[2] }
+        word.forEachIndexed { idx, ch ->
+            if (otherWords.none { other -> other.length > idx && other[idx] == ch }) {
+                return ch
+            }
+        }
+        return '.'
+    }
 
-    return thirdLetters.toSet()
+    return words.map { word -> findUnique(word, words - word) }.toSet()
 }
--- src/test/kotlin/mjs/NumbersKtTest.kt
+++ src/test/kotlin/mjs/NumbersKtTest.kt
@@ -5,10 +5,18 @@ import io.kotest.matchers.shouldBe
 
 class NumbersKtTest : DescribeSpec({
 
+    val weekdays = setOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    val months = setOf(
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    )
+
     describe("uniqueIds()") {
         it("returns unique letters for the days in the week") {
-            uniqueIds(WEEKDAYS) shouldBe setOf('M', 'e', 'W', 'h', 'F', 'a', 'n')
+            uniqueIds(weekdays) shouldBe setOf('M', 'e', 'W', 'h', 'F', 'a', '.')
+        }
+        it("returns unique letters for months of the year") {
+            uniqueIds(months) shouldBe setOf('a', 'F', 'c', 'p', 'y', '.', 'l', 'g', 'S', 'O', 'N', 'D')
         }
     }
-
 })
